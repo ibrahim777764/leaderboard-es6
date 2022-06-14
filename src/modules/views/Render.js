f716d332bdb989e68935c7395fe326ce3c819f06
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-06-14 15:10:51 +0500

    remove linters error

--- src/modules/views/Render.js
+++ src/modules/views/Render.js
@@ -3,17 +3,17 @@ export default class Render {
     const list = document.getElementById('list');
     list.innerHTML = `<li><p>${scores.name}: ${scores.score}</p></li>`;
   }
+
   static showAlert(message, className) {
     const div = document.createElement('div');
     div.className = `alert ${className}`;
     div.appendChild(document.createTextNode(message));
     const container = document.querySelector('.score-form');
     const form = document.getElementById('submit-data');
-  
     container.insertBefore(div, form);
-  setTimeout(() => {
-  document.querySelector('.alert').remove();
-      }, 3000);
-    }
+    setTimeout(() => {
+    document.querySelector('.alert').remove();
+    }, 3000);
   }
+}
 
